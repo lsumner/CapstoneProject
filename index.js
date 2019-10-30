0e5a9000c480e6272df74c0b243d90c88a8ff7b2
laura sumner <laurasumner@lauras-MacBook-Pro.local>
2019-10-29 20:18:22 -0500

    install and setup navigo

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import {Header, Nav, Main, Footer} from "./components";
-
+import Navigo from "navigo";
 import * as state from "./store";
+console.log(state.Home)
+
+const router = new Navigo(location.origin);
+
+function capitalize (word) {
+    const firstLetter = word[0];
+    const restOfTheLetters = word.slice(1);
+    return firstLetter.toUpperCase() + restOfTheLetters.toLowerCase();
+}
 
 function render (st = state.Home) {
+    console.log(st)
     document.querySelector("#root").innerHTML = `
     ${Header(st)}
     ${Nav(st)}
     ${Main(st)}
     ${Footer()}
-`;  
+`;
+router.updatePageLinks();  
 }
+
+router
+  .on(":page", params => {
+    const page = params.page;
+    const targetState = state[capitalize(page)];
+    render(targetState);
+  })
+  .on("/", () => render(state.Home))
+  .resolve();
